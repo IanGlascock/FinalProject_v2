86e4f7a2e70eef67d548b8560070c23d2a8882f7
Ian Glascock <ig.7157@gmail.com>
2014-08-12 12:36:55 -0400

    Minor Changes & organization

--- Assets/scripts/Scrolling/ScrollingBox.cs
+++ Assets/scripts/Scrolling/ScrollingBox.cs
@@ -4,18 +4,20 @@ using System.Collections;
 public class ScrollingBox : MonoBehaviour {
 
 	// allows for access public to var. which has context
-	public float speed = 5f;
+	float startSpeed = 1f;
+	float acceleration = 0.1f;
+	float maxSpeed = 5;
 	// allows changing speed for additional levels
 	public int level = 1;
 	// acces too variriable direction 
-	public Vector3 direction;
-	public float StopPosition = 95f; 
+	public Vector3 direction; 
+
 	
 	// Use this for initialization
 	void Start () {
 		//checks position of object 
 		Debug.Log (GetComponent<Transform>().position);	
-		Debug.Log ("Camera is moving");
+		Debug.Log ("Killbox is moving");
 	}
 	
 	
@@ -24,11 +26,15 @@ public class ScrollingBox : MonoBehaviour {
 		//                             add to sellf ammount         * change in time to adjust for dif. in FPS
 		//GetComponent<Transform>().position += new Vector3(1f,1f,0f) * Time.deltaTime * speed;
 		// makes the direction a variable that is editable in Unity
-		if (GetComponent<Transform>().position.x <= StopPosition) {
-			GetComponent<Transform>().position += direction * Time.deltaTime * speed;
+		if (GetComponent<Transform>().position.x <= 95f) {
+			GetComponent<Transform>().position += direction * Time.deltaTime * startSpeed;
+			if (startSpeed <= maxSpeed) { 
+				startSpeed += acceleration; 
+			}
+
 		}
 		else 
-			Debug.Log ("Camera stopped");
+			Debug.Log ("Killbox stopped");
 		// need to slowly increase speed as the level continues 
 		// need to stop the movement of the thing at the end of the level
 		
